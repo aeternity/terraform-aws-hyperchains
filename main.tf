e8cef9d7f59b1a425d0034365abc68b646152a8d
Dincho Todorov <dincho@users.noreply.github.com>
2025-01-23 15:12:15 +0200

    Add aehc_perf and aehc_dincho chains

--- main.tf
+++ main.tf
@@ -1,46 +1,10 @@
 ###
-# Parent chain for demo hyperchain
+# Hyperchain aehc_dincho
 ###
 
-module "nodes_aehc_demo_parent_stockholm" {
+module "nodes_aehc_dincho_stockholm" {
   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env               = "aehc_demo"
-
-  static_nodes   = 1
-  spot_nodes_min = 0
-  spot_nodes_max = 0
-
-  instance_type  = "m5.large"
-  instance_types = ["m6i.large", "m5.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
-
-  root_volume_size        = 20
-  additional_storage      = true
-  additional_storage_size = 40
-
-  asg_target_groups = module.lb_aehc_demo_parent_stockholm.target_groups
-
-  tags = {
-    role  = "aenode"
-    env   = "aehc_demo"
-    kind  = "parent"
-  }
-
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/aehc_demo_parent"
-  }
-
-  providers = {
-    aws = aws.eu-north-1
-  }
-}
-
-module "mdw_aehc_demo_parent_stockholm" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env               = "aehc_demo"
+  env               = "aehc_dincho"
 
   static_nodes   = 1
   spot_nodes_min = 0
@@ -54,24 +18,19 @@ module "mdw_aehc_demo_parent_stockholm" {
   additional_storage      = true
   additional_storage_size = 40
 
-  vpc_id  = module.nodes_aehc_demo_parent_stockholm.vpc_id
-  subnets = module.nodes_aehc_demo_parent_stockholm.subnets
-
-  enable_mdw = true
-
-  asg_target_groups = module.lb_aehc_demo_parent_stockholm.target_groups_mdw
+  asg_target_groups = module.lb_aehc_dincho_stockholm.target_groups
 
   tags = {
-    role  = "aemdw"
-    env   = "aehc_demo"
-    kind  = "parent"
+    role  = "aenode"
+    env   = "aehc_dincho"
+    kind  = "child"
   }
 
   config_tags = {
     vault_role        = "ae-node"
     vault_addr        = var.vault_addr
     bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/aehc_demo_parent_mdw"
+    bootstrap_config  = "secret2/aenode/config/aehc_dincho_validator"
   }
 
   providers = {
@@ -79,18 +38,59 @@ module "mdw_aehc_demo_parent_stockholm" {
   }
 }
 
-module "lb_aehc_demo_parent_stockholm" {
+module "mdw_aehc_dincho_stockholm" {
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+  env               = "aehc_dincho"
+
+  static_nodes   = 1
+  spot_nodes_min = 0
+  spot_nodes_max = 0
+
+  instance_type  = "t3.large"
+  instance_types = ["t3.large", "c5.large", "m5.large"]
+  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+
+  root_volume_size        = 20
+  additional_storage      = true
+  additional_storage_size = 40
+
+  vpc_id  = module.nodes_aehc_dincho_stockholm.vpc_id
+  subnets = module.nodes_aehc_dincho_stockholm.subnets
+
+  enable_mdw = true
+
+  asg_target_groups = module.lb_aehc_dincho_stockholm.target_groups_mdw
+
+  tags = {
+    role  = "aemdw"
+    env   = "aehc_dincho"
+    kind  = "child"
+  }
+
+  config_tags = {
+    vault_role        = "ae-node"
+    vault_addr        = var.vault_addr
+    bootstrap_version = var.bootstrap_version
+    bootstrap_config  = "secret2/aenode/config/aehc_dincho_mdw"
+  }
+
+  providers = {
+    aws = aws.eu-north-1
+  }
+}
+
+module "lb_aehc_dincho_stockholm" {
   source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v2.0.0"
-  env                       = "aehc_demo"
-  fqdn                      = var.lb_fqdn_parent
+  env                       = "aehc_dincho"
+  fqdn                      = var.lb_fqdn_dincho
   dns_zone                  = var.dns_zone
-  security_group            = module.nodes_aehc_demo_parent_stockholm.sg_id
-  mdw_security_group        = module.mdw_aehc_demo_parent_stockholm.sg_id
-  vpc_id                    = module.nodes_aehc_demo_parent_stockholm.vpc_id
-  subnets                   = module.nodes_aehc_demo_parent_stockholm.subnets
+  security_group            = module.nodes_aehc_dincho_stockholm.sg_id
+  mdw_security_group        = module.mdw_aehc_dincho_stockholm.sg_id
+  vpc_id                    = module.nodes_aehc_dincho_stockholm.vpc_id
+  subnets                   = module.nodes_aehc_dincho_stockholm.subnets
 
   enable_ssl                = true
-  certificate_arn           = var.certificate_arn
+  certificate_arn           = var.certificate_arn_dincho
 
   internal_api_enabled      = true
   state_channel_api_enabled = false
@@ -102,8 +102,114 @@ module "lb_aehc_demo_parent_stockholm" {
   }
 }
 
+
 ###
-# Hyperchain aechc_demo
+# Hyperchain aehc_perf
+###
+
+module "nodes_aehc_perf_stockholm" {
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+  env               = "aehc_perf"
+
+  static_nodes   = 1
+  spot_nodes_min = 0
+  spot_nodes_max = 0
+
+  instance_type  = "t3.large"
+  instance_types = ["t3.large", "c5.large", "m5.large"]
+  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+
+  root_volume_size        = 20
+  additional_storage      = true
+  additional_storage_size = 40
+
+  asg_target_groups = module.lb_aehc_perf_stockholm.target_groups
+
+  tags = {
+    role  = "aenode"
+    env   = "aehc_perf"
+    kind  = "child"
+  }
+
+  config_tags = {
+    vault_role        = "ae-node"
+    vault_addr        = var.vault_addr
+    bootstrap_version = var.bootstrap_version
+    bootstrap_config  = "secret2/aenode/config/aehc_perf_validator"
+  }
+
+  providers = {
+    aws = aws.eu-north-1
+  }
+}
+
+module "mdw_aehc_perf_stockholm" {
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+  env               = "aehc_perf"
+
+  static_nodes   = 1
+  spot_nodes_min = 0
+  spot_nodes_max = 0
+
+  instance_type  = "t3.large"
+  instance_types = ["t3.large", "c5.large", "m5.large"]
+  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+
+  root_volume_size        = 20
+  additional_storage      = true
+  additional_storage_size = 40
+
+  vpc_id  = module.nodes_aehc_perf_stockholm.vpc_id
+  subnets = module.nodes_aehc_perf_stockholm.subnets
+
+  enable_mdw = true
+
+  asg_target_groups = module.lb_aehc_perf_stockholm.target_groups_mdw
+
+  tags = {
+    role  = "aemdw"
+    env   = "aehc_perf"
+    kind  = "child"
+  }
+
+  config_tags = {
+    vault_role        = "ae-node"
+    vault_addr        = var.vault_addr
+    bootstrap_version = var.bootstrap_version
+    bootstrap_config  = "secret2/aenode/config/aehc_perf_mdw"
+  }
+
+  providers = {
+    aws = aws.eu-north-1
+  }
+}
+
+module "lb_aehc_perf_stockholm" {
+  source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v2.0.0"
+  env                       = "aehc_perf"
+  fqdn                      = var.lb_fqdn_perf
+  dns_zone                  = var.dns_zone
+  security_group            = module.nodes_aehc_perf_stockholm.sg_id
+  mdw_security_group        = module.mdw_aehc_perf_stockholm.sg_id
+  vpc_id                    = module.nodes_aehc_perf_stockholm.vpc_id
+  subnets                   = module.nodes_aehc_perf_stockholm.subnets
+
+  enable_ssl                = true
+  certificate_arn           = var.certificate_arn_perf
+
+  internal_api_enabled      = true
+  state_channel_api_enabled = false
+  mdw_enabled               = true
+  dns_health_check          = false
+
+  providers = {
+    aws = aws.eu-north-1
+  }
+}
+
+
+###
+# Hyperchain aehc_demo
 ###
 
 module "nodes_aehc_demo_stockholm" {
@@ -141,7 +247,6 @@ module "nodes_aehc_demo_stockholm" {
     aws = aws.eu-north-1
   }
 }
-
 
 module "mdw_aehc_demo_stockholm" {
   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
@@ -187,7 +292,7 @@ module "mdw_aehc_demo_stockholm" {
 module "lb_aehc_demo_stockholm" {
   source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v2.0.0"
   env                       = "aehc_demo"
-  fqdn                      = var.lb_fqdn
+  fqdn                      = var.lb_fqdn_demo
   dns_zone                  = var.dns_zone
   security_group            = module.nodes_aehc_demo_stockholm.sg_id
   mdw_security_group        = module.mdw_aehc_demo_stockholm.sg_id
@@ -195,7 +300,7 @@ module "lb_aehc_demo_stockholm" {
   subnets                   = module.nodes_aehc_demo_stockholm.subnets
 
   enable_ssl                = true
-  certificate_arn           = var.certificate_arn
+  certificate_arn           = var.certificate_arn_demo
 
   internal_api_enabled      = true
   state_channel_api_enabled = false
--- variables.tf
+++ variables.tf
@@ -10,14 +10,26 @@ variable "dns_zone" {
   default = "ZSEEAAX46MKWZ"
 }
 
-variable "lb_fqdn" {
+variable "lb_fqdn_demo" {
   default = "demo.hyperchains.aeternity.io"
 }
 
-variable "lb_fqdn_parent" {
-  default = "demo-parent.hyperchains.aeternity.io"
+variable "certificate_arn_demo" {
+  default = "arn:aws:acm:eu-north-1:106102538874:certificate/49b2912b-5269-4e00-84ee-cd6f0f2c7627"
 }
 
-variable "certificate_arn" {
+variable "lb_fqdn_dincho" {
+  default = "dincho.hyperchains.aeternity.io"
+}
+
+variable "certificate_arn_dincho" {
+  default = "arn:aws:acm:eu-north-1:106102538874:certificate/49b2912b-5269-4e00-84ee-cd6f0f2c7627"
+}
+
+variable "lb_fqdn_perf" {
+  default = "perf.hyperchains.aeternity.io"
+}
+
+variable "certificate_arn_perf" {
   default = "arn:aws:acm:eu-north-1:106102538874:certificate/49b2912b-5269-4e00-84ee-cd6f0f2c7627"
 }
